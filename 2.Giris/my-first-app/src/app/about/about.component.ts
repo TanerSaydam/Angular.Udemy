20c5519660a2f350efb4772bc44c4074957859cb
TanerSaydam <tanersaydam@gmail.com>
2024-06-11 02:24:35 +0300

    Giris - Component nedir?

---
+++ 2.Giris/my-first-app/src/app/about/about.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component({
+    standalone: true,
+    templateUrl: "./about.component.html",
+    styleUrl: "./about.component.css",
+    imports:[],
+    providers:[],
+    encapsulation: ViewEncapsulation.None
+})
+export class AboutComponent{
+
+}
---
+++ 2.Giris/my-first-app/src/app/contact/contact.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    template: "<h1>Contact page</h1>"
+})
+export class ContactComponent{
+    
+}
