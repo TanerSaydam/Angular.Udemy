65bb02a137e694517ac9e49ea1d51b361bf30ea3
TanerSaydam <tanersaydam@gmail.com>
2024-06-11 03:13:15 +0300

    Giris - Two Data Binding

--- 2.Giris/my-first-app/src/app/app.component.ts
+++ 2.Giris/my-first-app/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, AboutComponent, ContactComponent],
+  imports: [RouterOutlet, HomeComponent, AboutComponent, ContactComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
