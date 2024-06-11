3e93f85b82a00eb0c228489f5234f2756b9814cb
TanerSaydam <tanersaydam@gmail.com>
2024-06-11 03:25:30 +0300

    OrnekProjeler - Yeni bir proje oluşturalım - todo-app

---
+++ 3.OrnekProjeler/todo-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  template: `
+  <h1>Todo App</h1>
+  
+  
+  `
+})
+export class AppComponent {
+}
