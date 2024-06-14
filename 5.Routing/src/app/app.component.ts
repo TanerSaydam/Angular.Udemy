91427b6eabc5bf079490f4a0498942bb646eb53b
TanerSaydam <tanersaydam@gmail.com>
2024-06-15 02:41:30 +0300

    Routing - Layout yapısı kurma ve routing ile yönetme

--- 5.Routing/src/app/app.component.ts
+++ 5.Routing/src/app/app.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
-  template: `
-  <nav>
-  <a routerLink="/home">Home Page</a> ||
-  <a routerLink="/about">About Page</a> ||
-  <a routerLink="/contact">Contact Page</a> ||    
-  </nav>
+  imports: [RouterOutlet],
+  template: `  
    <router-outlet></router-outlet>
   `
 })
--- 5.Routing/src/app/app.routes.ts
+++ 5.Routing/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Route, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LayoutsComponent } from './layouts/layouts.component';
+import { LoginComponent } from './login/login.component';
 
 export const homeRoute: Route =  {
     path: "",
@@ -9,23 +11,33 @@ export const homeRoute: Route =  {
 }
 
 export const routes: Routes = [
-    homeRoute,
     {
-        path: "home",
-        component: HomeComponent
+        path: "login",
+        component: LoginComponent
     },
     {
-        path: "about",
-        component: AboutComponent
-    },
-    {
-        path: "contact",
-        component: ContactComponent
-    },
-    {
-        path: "contact/:params",
-        component: ContactComponent
-    }
+        path: "",
+        component: LayoutsComponent,
+        children: [
+            homeRoute,
+            {
+                path: "home",
+                component: HomeComponent
+            },
+            {
+                path: "about",
+                component: AboutComponent
+            },
+            {
+                path: "contact",
+                component: ContactComponent
+            },
+            {
+                path: "contact/:params",
+                component: ContactComponent
+            }
+        ]
+    }    
 ];
 
 
