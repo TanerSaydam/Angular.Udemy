ac1198c2fb537ed4cbc12dbb66d1f781c410f385
TanerSaydam <tanersaydam@gmail.com>
2024-06-15 02:29:25 +0300

    Routing - Function içerisinde sayfalar arası geçiş

--- 5.Routing/src/app/home/home.component.ts
+++ 5.Routing/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(
+    private router: Router
+  ){}
 
+  method(){
+    this.router.navigate(["about"]);
+    this.router.navigateByUrl("/about");
+  }
 }
