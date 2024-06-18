662d38918d8ef8ff37c6b9b324cc84cb1fe558f3
TanerSaydam <tanersaydam@gmail.com>
2024-06-19 02:24:29 +0300

    Pipe - Angular pipes

--- 8.Pipe/src/app/app.component.ts
+++ 8.Pipe/src/app/app.component.ts
@@ -1,37 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { TodoPipe } from './todo.pipe';
-import { NamePipe } from './name.pipe';
+import { TrCurrencyPipe } from 'tr-currency';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, TodoPipe, NamePipe],
+  imports: [CommonModule, TrCurrencyPipe],
   template:`
   <h1>Pipe</h1>
-  <div>
-    <input [(ngModel)]="work">
-    <button (click)="save()">Save</button>
-  </div>
-  <h1>{{name | name}}</h1>
-  <div>
-    <input type="search" [(ngModel)]="search" placeholder="Search something...">
-    <ul>
-      @for(t of todos | todo:search;track t){
-        <li>{{t}}</li>
-      }      
-    </ul>
-  </div>
+  <h1>{{name | titlecase | uppercase | lowercase}}</h1>
+  <h1>{{date | date:'dd.MMMM.yyy HH:mm:ss'}}</h1>
+  <h1>{{num | currency:'EUR':'symbol-narrow':'1.2-2'}}</h1>
+  <h1>{{num | trCurrency: '₺': true}}</h1>
   `
 })
 export class AppComponent {
-  name: string = "Taner"
-  work: string = "";
-  todos: string[] = ["Domates","Armut","Elma"];
-  search: string = "";
-
-  save(){
-    this.todos.push(this.work);
-    this.work = "";
-  }
+  name: string = "taner Saydam";
+  date: Date = new Date();
+  num: number = 17002.25;
 }
