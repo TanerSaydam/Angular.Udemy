7e40adf160c89ed8d58a8c2c8e86d88163af118a
TanerSaydam <tanersaydam@gmail.com>
2024-06-19 02:36:05 +0300

    Guard - Guard nedir?

---
+++ 9.Guard/src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  constructor(
+    private router: Router
+  ){}
+
+  login(){
+    this.router.navigateByUrl("/");
+  }
+}
