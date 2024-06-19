eb238f7f500e5b063625b59b1add8e2c2714d1df
TanerSaydam <tanersaydam@gmail.com>
2024-06-19 03:27:25 +0300

    Http - Yeni versiyonlar için not

---
+++ 10.Http/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
+  template:`<router-outlet></router-outlet>`
+})
+export class AppComponent {}
